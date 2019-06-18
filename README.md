8bb5f996bba8ac0a6bb023bb3b8e1537c88ef8dd
Colin Lyons <myndsai@Colins-MacBook-Pro.local>
2019-06-18 16:41:50 -0700

    added comments and updated the README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Liri-Bot
+LIRI is like iPhone's SIRI, however, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI is be a command line node app that takes in on of four parameters and returns the relative data.
+ spotify - events - movie
+
+Deployment
+Clone repo
+Run npm install
+At command prompt run node liri.js <pass in an instruction from above>
--- javascript.js
+++ javascript.js
@@ -5,76 +5,75 @@ const axios = require('axios');
 var Spotify = require("node-spotify-api");
 
 
+// Assigning argumenents within the array for Node
+
+// assigning the string 'spotify' to the 3rd argument in node
 if (process.argv[2] === 'spotify') {
 
   spotifySong(process.argv[3]);
 
 }
 
+// assigning the string 'events' to the 3rd argument in node
 if (process.argv[2] === 'events') {
 
   bandTown(process.argv[3]);
 
 }
 
-
-
+// assigning the string 'movies' to the 3rd argument in node
 if (process.argv[2] === 'movies') {
 
-    movies(process.argv[3]);
+  movies(process.argv[3]);
 
 }
 
+
+
+// this function uses an api call and uses the 4th argument in node as the query.
 function spotifySong() {
 
-var spotify = new Spotify({
-  id: "b2592c6c2d464c2398be62f4c9489d11",
-  secret: "22198b559f034d83912bda7c9ae25b8e"
-});
+  var spotify = new Spotify({
+    id: "b2592c6c2d464c2398be62f4c9489d11",
+    secret: "22198b559f034d83912bda7c9ae25b8e"
+  });
 
-spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log("---",data.tracks.items[0]); 
-
-});
- 
-
-
+  spotify.search({
+    type: 'track',
+    query: process.argv[3]
+  }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    console.log("---", data.tracks.items[0]);
+  });
 }
 
-function bandTown() {
+
+// this function uses an api call and uses the 4th argument in node as the query.
+function bandTown(artist) {
 
   axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-  .then(function(response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
 
 }
 
+// this function uses an api call and uses the 4th argument in node as the query.
 function movies(movieTitle) {
 
-
-    axios.get(' http://www.omdbapi.com/?t='+movieTitle+'&apikey=7d844e63')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-
-
+  axios.get(' http://www.omdbapi.com/?t=' + movieTitle + '&apikey=7d844e63')
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
 }
-
-
-
-
